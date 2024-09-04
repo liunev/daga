010e98bfaa76fd4acbfff81dcb37cefa15c7fe84
liunev <linunev@liunev.com>
2024-09-03 18:27:20 -0600

    Set hello as default page

--- server.lisp
+++ server.lisp
@@ -1,6 +1,16 @@
-(ql:quickload '(cl-who hunchentoot parenscript elephant fiveam css-lite))
+(ql:quickload '(hunchentoot))
 (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242))
 
-(hunchentoot:define-easy-handler (say-hello :uri "/hello") (name)
+(hunchentoot:define-easy-handler (say-hello :uri "/") ()
  (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Hello, ~a! I am Liunev! ~%I build a website with Lisp!!!" name))
+ (format nil "Hello, ~a! I am Liunev! ~%I build a website with Lisp!!!" "test"))
+
+;;(hunchentoot:define-easy-handler (create-task :uri "/task")
+ ;;   (setf (hunchentoot:content-type*) "text/html")
+  ;;(let ((request-type (hunchentoot:request-method hunchentoot:*request*)))
+   ;; (cond ((eq request-type :get) (format nil "Test my task")));; handle get request
+    ;;      ((eq request-type :post)
+     ;;      (let* ((data-string (hunchentoot:raw-post-data :force-text t))
+      ;;            (json-obj (jsown:parse data-string))) ;; use jsown to parse the string
+       ;;        .... ;; play with json-obj
+        ;;       data-string)))))
